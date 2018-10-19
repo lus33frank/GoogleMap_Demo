757f9ac3649b1ee2ed56784da9ac3345dbce664f
lus33frank <lus33frank@gmail.com>
2018-10-20 01:16:00 +0800

    Commit : 1    練習完成。

--- app/src/main/java/com/frankchang/googlemap_demo/MapsActivity.java
+++ app/src/main/java/com/frankchang/googlemap_demo/MapsActivity.java
@@ -1,23 +1,39 @@
 package com.frankchang.googlemap_demo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_LOCATION = 900;
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,8 +55,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        setupMap();
     }
+
+    private void setupMap() {
+        // 台北 101 經緯度 ： 25.0339687,121.5622835
+
+        // 設定經緯度物件
+        LatLng taipei101 = new LatLng(25.0339687, 121.5622835);
+        // 設定地圖顯示畫面位置和縮放比率
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(taipei101, 18f));
+        // 設定標示圖釘和備註說明
+        mMap.addMarker(new MarkerOptions()
+                .position(taipei101)
+                .title("這裡是台北 101"));
+
+        // 詢問危險權限，以開啟相關功能
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+
+            return;
+        }
+
+        // 設定相關功能
+        setupLocation();
+    }
+
+    // 設定相關功能
+    @SuppressLint("MissingPermission")
+    private void setupLocation() {
+        // 我的位置按鈕功能：開啟
+        mMap.setMyLocationEnabled(true);
+        // 地圖縮放功能：開啟
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // 取得行動裝置目前所在位置經緯度
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    String myLocation = location.getLatitude() + "," + location.getLongitude();
+                    Log.d(TAG, "onComplete: " + myLocation);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setupLocation();
+        }
+    }
+
 }
